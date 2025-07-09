e52fd2e754b3dbc1135a65e5beae970083bf82e5
McNaDan <macennadaniel@gmail.com>
2025-07-09 13:13:11 -0300

    nome do arquivoUsuario.js para usuario.js no import

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const cors = require('cors');
 
-const Usuario = require('./models/Usuario');
-const Habito = require('./models/Habito');
-const Registro = require('./models/Registro');
-const Categoria = require('./models/Categoria');
+const Usuario = require('./models/usuario');
+const Habito = require('./models/habito');
+const Registro = require('./models/registro');
+const Categoria = require('./models/categoria');
 
 // Habilita o CORS para todas as origens (pode ser customizado para origens específicas)
 app.use(cors());
 app.use(express.json()); // Para que o Express consiga entender JSON no corpo da requisição
 
 // Configuração do MongoDB
-const MONGO_URI = 'mongodb+srv://danielm26:newsenhamongodb@habitsappcluster.lhwikak.mongodb.net/?retryWrites=true&w=majority&appName=HabitsAppCluster'; // Substitua pela URI do MongoDB Atlas se necessário
+const MONGO_URI = process.env.MONGODB_URI; 
+//'mongodb+srv://danielm26:newsenhamongodb@habitsappcluster.lhwikak.mongodb.net/?retryWrites=true&w=majority&appName=HabitsAppCluster'; // Substitua pela URI do MongoDB Atlas se necessário
 
 // Conectando ao MongoDB
 mongoose.connect(MONGO_URI)
